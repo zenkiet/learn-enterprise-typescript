5b461c26aac5e609dca8008251ca23dd2f3ed29a
Zen Le <kietgolx65234@gmail.com>
2024-08-03 23:41:17 +0700

    build example jest for testing
    reference:
    - https://typescript-eslint.io/packages/eslint-plugin
    use command:
    - yarn build
    - yarn test

--- chat-stdlib/eslint.config.mjs
+++ chat-stdlib/eslint.config.mjs
@@ -2,39 +2,26 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-import { fileURLToPath } from "url";
-import { dirname } from "path";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 export default [
   { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.strictTypeChecked,
   {
     languageOptions: {
+      globals: { ...globals.browser, ...globals.node, ...globals.es2023 },
       parserOptions: {
         ecmaVersion: "latest",
         project: true,
-        tsconfigRootDir: __dirname,
+        tsconfigDirName: import.meta.dirname,
+        sourceType: "module",
       },
     },
   },
   {
-    rules: {
-      "prefer-const": "error",
-      "@typescript-eslint/no-unused-vars": "off",
-      "@typescript-eslint/no-unused-params": "off",
-    },
-  },
-  {
     files: ["tests/**/*.ts"],
-    ...tseslint.configs.disableTypeChecked,
-    env: { node: true, jest: true },
-    rules: {
-      "@typescript-eslint/no-unsafe-assignment": "off",
+    languageOptions: {
+      globals: { ...globals.node, ...globals.jest },
     },
+    ...tseslint.configs.disableTypeChecked,
   },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
 ];
