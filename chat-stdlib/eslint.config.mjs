6467c815cdb726210ff348b72a8a593de8384593
Zen Le <kietgolx65234@gmail.com>
2024-08-03 17:28:50 +0700

    configure the eslint.config.mjs
    reference:
    - https://www.raulmelo.me/en/blog/migration-eslint-to-flat-config
    use command to test:
    - yarn lint
    => get error:
      67:24  error  Invalid operand for a '+' operation. Operands must each be a number or string. Got `{}`      @typescript-eslint/restrict-plus-operands
      67:24  error  'err' will use Object's default stringification format ('[object Object]') when stringified  @typescript-eslint/no-base-to-string

--- chat-stdlib/eslint.config.mjs
+++ chat-stdlib/eslint.config.mjs
@@ -2,10 +2,39 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  ...tseslint.configs.strictTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: "latest",
+        project: true,
+        tsconfigRootDir: __dirname,
+      },
+    },
+  },
+  {
+    rules: {
+      "prefer-const": "error",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-unused-params": "off",
+    },
+  },
+  {
+    files: ["tests/**/*.ts"],
+    ...tseslint.configs.disableTypeChecked,
+    env: { node: true, jest: true },
+    rules: {
+      "@typescript-eslint/no-unsafe-assignment": "off",
+    },
+  },
 ];
